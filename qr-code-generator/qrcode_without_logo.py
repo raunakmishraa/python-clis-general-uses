fc613b47f8065ae81beb21be5b4c9ae173c12e7f
Raunak Kumar <71320898+raunakmishraa@users.noreply.github.com>
2025-12-11 21:59:52 +0545

    Update qrcode_without_logo.py

--- qr-code-generator/qrcode_without_logo.py
+++ qr-code-generator/qrcode_without_logo.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image
 
 
-link='https://forms.gle/PoxrY9TmuZb9pGot9'
+link='https://forms.gle/PoxrY9TmuZb9pGot9' #change the link
 
 QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
 QRcode.add_data(link)
@@ -12,4 +12,5 @@ QRcolor = '#fa0096'
 QRimg = QRcode.make_image(fill_color=QRcolor, back_color="transparent")
 save_as_name="D:\\Innovation\\Python_Programs\\Python_CLIs\\entranceWorldQR.png"
 QRimg.save(save_as_name)
+
 print("QR Generated !!")
