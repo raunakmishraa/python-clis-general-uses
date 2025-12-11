293aee2500b830ca5fbd91c0629b7914f324bb6d
Raunak Kumar <71320898+raunakmishraa@users.noreply.github.com>
2025-12-11 21:59:14 +0545

    Add files via upload

---
+++ qr-code-generator/qrcode_without_logo.py
@@ -0,0 +1,15 @@
+import qrcode
+from PIL import Image
+
+
+link='https://forms.gle/PoxrY9TmuZb9pGot9'
+
+QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+QRcode.add_data(link)
+QRcode.make()
+#Change QR color
+QRcolor = '#fa0096'
+QRimg = QRcode.make_image(fill_color=QRcolor, back_color="transparent")
+save_as_name="D:\\Innovation\\Python_Programs\\Python_CLIs\\entranceWorldQR.png"
+QRimg.save(save_as_name)
+print("QR Generated !!")
